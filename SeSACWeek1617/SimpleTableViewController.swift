7739e53d0a526803d40804b6cc2cd7bfdfd39727
지운 <vonreal20@gmail.com>
2022-10-18 09:59:55 +0900

    Initial Commit

---
+++ SeSACWeek1617/SimpleTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SeSACWeek1617
+//
+//  Created by 나지운 on 2022/10/18.
+//
+
+import UIKit
+
+class SimpleTableViewController: UITableViewController {
+
+    let list = ["슈비버거", "프랭크", "자갈치", "고래밥"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
