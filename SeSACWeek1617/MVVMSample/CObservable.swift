f48ef0ad62115043cf5c2228a742992306a722eb
지운 <vonreal20@gmail.com>
2022-10-20 09:41:53 +0900

    mvvm 실습 - observable 생성

---
+++ SeSACWeek1617/MVVMSample/CObservable.swift
@@ -0,0 +1,32 @@
+//
+//  CObservable.swift
+//  SeSACWeek1617
+//
+//  Created by 나지운 on 2022/10/20.
+//
+
+import Foundation
+
+/*
+ Observable
+ */
+
+class CObservable<T> {
+    private var listener: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ closure: @escaping (T) -> Void) {
+        closure(value)
+        listener = closure
+    }
+    
+}
