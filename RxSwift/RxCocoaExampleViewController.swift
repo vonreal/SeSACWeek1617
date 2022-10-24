b795cd6bbd60131a3a4dd1f4d41a6cf7523bd770
지운 <vonreal20@gmail.com>
2022-10-24 10:59:19 +0900

    RxSwift - 2: RxCocoa를 통한 테이블 뷰 예제 코드 실습

--- RxSwift/RxCocoaExampleViewController.swift
+++ RxSwift/RxCocoaExampleViewController.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift // Cannot find 'Observable' in scope.
+
 class RxCocoaExampleViewController: UIViewController {
 
     @IBOutlet weak var simpleTableView: UITableView!
     @IBOutlet weak var simplePickerView: UIPickerView!
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +26,22 @@ class RxCocoaExampleViewController: UIViewController {
     
     func setTableView() {
         
+        simpleTableView.register(UITableView.self, forCellReuseIdentifier: "Cell")
+        
+        // RxSwift/UITableView+Rx의 Example!
+        let items = Observable.just([
+            "First Item",
+            "Second Item",
+            "Third Item"
+        ])
+
+        items
+        .bind(to: simpleTableView.rx.items) { (tableView, row, element) in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+            cell.textLabel?.text = "\(element) @ row \(row)"
+            return cell
+        }
+        .disposed(by: disposeBag) // 구독 취소
     }
     
     func setPickerView() {
