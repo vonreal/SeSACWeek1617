23d3e3a9997d23a0fb716b04c068940d526a5152
지운 <vonreal20@gmail.com>
2022-10-24 10:51:16 +0900

    RxSwift - 1: 뷰컨, 파일 생성

---
+++ RxSwift/RxCocoaExampleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RxCocoaExampleViewController.swift
+//  SeSACWeek1617
+//
+//  Created by 나지운 on 2022/10/24.
+//
+
+import UIKit
+
+class RxCocoaExampleViewController: UIViewController {
+
+    @IBOutlet weak var simpleTableView: UITableView!
+    @IBOutlet weak var simplePickerView: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setTableView()
+        setPickerView()
+    }
+    
+    func setTableView() {
+        
+    }
+    
+    func setPickerView() {
+        
+    }
+}
